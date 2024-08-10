b4fb14e660485cdc01e599f1cb5394b6ece171b0
EepyCapy <EepyCapy@gmail.com>
2024-08-10 18:40:27 +0300

    Modified Linear Regression for DearPyGui

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,17 +1,10 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
 
 class LinearRegression():
-    def __init__(self): #, xData, yData):
-        self.xData = np.array([])
-        self.yData = np.array([])
+    def __init__(self, xData, yData):
+        self.xData = xData
+        self.yData = yData
         self.parameters = np.array([])
-        self.fig = plt.figure()
-        if self.xData.ndim == 1:
-            self.ax = self.fig.add_axes()
-        elif self.xData.ndim == 2:
-            self.ax = self.fig.add_subplot(projection='3d')
 
     def multidLS(self):
         if self.xData.ndim == 1:
@@ -21,25 +14,25 @@ class LinearRegression():
         vector = X.T @ np.reshape(self.yData, (len(self.yData), 1))
         self.parameters = np.linalg.solve(matrix, vector)
 
-    def plotData(self):
-        if self.xData.ndim == 1:
-            self.ax.scatter(self.xData, self.yData)
-        elif self.xData.ndim == 2:
-            self.ax.scatter(self.xData[:, 0], self.xData[:, 1], self.yData)
-        else:
-            print('Cannot plot more than 3 dimensions')
+    #def plotData(self):
+    #    if self.xData.ndim == 1:
+    #        self.ax.scatter(self.xData, self.yData)
+    #    elif self.xData.ndim == 2:
+    #        self.ax.scatter(self.xData[:, 0], self.xData[:, 1], self.yData)
+    #    else:
+    #        print('Cannot plot more than 3 dimensions')
 
     def plotPrediction(self):
-        if self.xData.ndim == 1:
-            yPred = self.parameters[0] + self.parameters[1] * self.xData
-            self.ax.plot(self.xData, yPred)
-        elif self.xData.ndim == 2:
+        if len(self.parameters) == 2:
+            return self.parameters[0] + self.parameters[1] * self.xData
+        elif self.xData.ndim == 3:
             x1, x2 = np.meshgrid(self.xData[:, 0], self.xData[:, 1])
-            yPred = self.parameters[0] + self.parameters[1] * x1 + self.parameters[2] * x2
-            self.ax.plot_surface(x1, x2, yPred, color='purple')
+            return self.parameters[0] + self.parameters[1] * x1 + self.parameters[2] * x2
+            #self.ax.plot_surface(x1, x2, yPred, color='purple')
         else:
             print('Cannot plot more than 3 dimensions')
-
+            return np.array([])
+            
 # Formulas for Simple Linear Regression
 #def calculateParameters(xData, yData):
 #    matrix = np.array([[len(xData)    , np.sum(xData)],
