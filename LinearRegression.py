85aa8fc1d25933f5221ad07a5a46430f577edaa2
EepyCapy <EepyCapy@gmail.com>
2024-08-19 02:31:09 +0300

    Modified Version of LinearRegression class

--- LinearRegression.py
+++ LinearRegression.py
@@ -7,36 +7,14 @@ class LinearRegression():
         self.parameters = np.array([])
 
     def multidLS(self):
-        if self.xData.ndim == 1:
-            self.xData = np.reshape(self.xData, (len(self.xData), 1))
+        print(self.xData)
         X = np.insert(self.xData, 0, np.ones(len(self.yData)), axis=1)
         matrix = X.T @ X
-        vector = X.T @ np.reshape(self.yData, (len(self.yData), 1))
+        vector = X.T @ self.yData
         self.parameters = np.linalg.solve(matrix, vector)
 
-    #def plotData(self):
-    #    if self.xData.ndim == 1:
-    #        self.ax.scatter(self.xData, self.yData)
-    #    elif self.xData.ndim == 2:
-    #        self.ax.scatter(self.xData[:, 0], self.xData[:, 1], self.yData)
-    #    else:
-    #        print('Cannot plot more than 3 dimensions')
-
-    def plotPrediction(self):
-        if len(self.parameters) == 2:
-            return self.parameters[0] + self.parameters[1] * self.xData
-        elif self.xData.ndim == 3:
-            x1, x2 = np.meshgrid(self.xData[:, 0], self.xData[:, 1])
-            return self.parameters[0] + self.parameters[1] * x1 + self.parameters[2] * x2
-            #self.ax.plot_surface(x1, x2, yPred, color='purple')
-        else:
-            print('Cannot plot more than 3 dimensions')
-            return np.array([])
-            
-# Formulas for Simple Linear Regression
-#def calculateParameters(xData, yData):
-#    matrix = np.array([[len(xData)    , np.sum(xData)],
-#                       [ np.sum(xData), np.sum(xData * xData)]])
-#    vector = np.array([[np.sum(yData)],
-#                       [np.sum(xData * yData)]])
-#    return np.linalg.solve(matrix, vector)
+    def predictedValues(self, axis):
+        yPred = self.parameters[0] * np.ones(len(self.yData))
+        yPred = yPred + self.parameters[axis + 1] * self.xData[:, axis]
+        print(yPred)
+        return yPred
